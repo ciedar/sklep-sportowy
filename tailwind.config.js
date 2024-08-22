2c0b478ae6eb13c633ed2dfa5d100d66ed51af02
ciedar <dciesla545@gmail.com>
2024-08-22 06:56:51 +0200

    addedImagesNewBreakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+          screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      xxl: '1744px'
+    },
+
+  height: {
+    xl: '500px'
+  },
+      backgroundImage: {
+        'hero-image': "url('src/images/Rectangle 1.png')"
+      }
+    },
   },
   plugins: [],
 }
