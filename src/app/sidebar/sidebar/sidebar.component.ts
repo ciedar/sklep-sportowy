9e04841d1183a8a4be7ea6d38887f78d54168f80
ciedar <dciesla545@gmail.com>
2024-11-07 20:10:42 +0100

    dropdownToSidebar

--- src/app/sidebar/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar/sidebar.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isOpen:any  = {
+    filterSort: false,
+    sortBy: false,
+    productType: false,
+    size: false,
+    features: false,
+    fit: false,
+    activity: false,
+    collection: false,
+    color: false,
+    pattern: false,
+    discount: false
+  };
 
+  toggle(section: string) {
+    this.isOpen[section] = !this.isOpen[section];
+  }
 }
